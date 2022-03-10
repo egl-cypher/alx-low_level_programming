bee7816de03159606367a7db7008a7aaa206b49b
egl-cypher <ransomq4@gmail.com>
2022-03-10 23:10:21 +0000

    made changes to 4 added \n

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
-/*  
- *using the put function to print a text
- *  */
 
-int main(void){
-
- put("\"Programming is like building a multilingual puzzle");
-
-  return(0);
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
+
+
+
+
+
+
+
+
+
