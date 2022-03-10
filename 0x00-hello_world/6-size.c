68b41c18b492cf14078279a46d5b5fb3c7eab4d9
egl-cypher <ransomq4@gmail.com>
2022-03-10 23:03:53 +0000

    printf to display datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio>
+
+/*
+ *using print to format and print sizes of data types
+ * */
+
+int main(void){
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a longint: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+
+  return(0);
+
+}
